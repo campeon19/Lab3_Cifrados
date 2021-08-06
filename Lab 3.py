52c212adff1633e4e4d126c66a8572a671c2edd8
campeon19 <53914171+campeon19@users.noreply.github.com>
2021-08-05 20:41:42 -0600

    Primer Commit

---
+++ Lab 3.py
@@ -0,0 +1,9 @@
+# Universidad del Valle de Guatemala
+# Cifrado de Información
+# Laboratorio 3
+#
+# Andrei Portales 19🏓🏓🏓 (Todavia no me lo se :v)
+# Jose Javier Hurtarte 19707
+# Christian Pérez 19710
+
+import numpy
